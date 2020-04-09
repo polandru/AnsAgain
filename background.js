a88d4d26c2583febe2b06786c90bff883dedb761
polandru <paul.andru.p@gmail.com>
2020-04-08 20:04:39 -0500

    Adding popup with functioning buttons

---
+++ background.js
@@ -0,0 +1,10 @@
+let save = document.getElementById("save");
+let fill = document.getElementById("fill");
+
+save.addEventListener("click",(e) =>{
+    console.log("save");
+});
+
+fill.addEventListener("click",(e) =>{
+    console.log("fill");
+});
